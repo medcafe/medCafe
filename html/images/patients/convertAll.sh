1c03506ada6c5ebea0fef593182163a9f95e75cf
Jeffrey Hoyt <jchoyt@mitre.org>
2010-05-14 19:16:11 +0000

    added PDFs for smoking, etc. and thumbnails.

---
+++ html/images/patients/convertAll.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+for f in `find . -name "*gif"`
+do
+  echo "Processing $f file..."
+  # take action on each file. $f store current file name
+  # convert -thumbnail 300x300 756_UNKNOWN.txt.pdf[0] thumb.png
+  name=${f%\.*}
+  convert -thumbnail 300x300 ${f}[0] ${name}_thumb.png
+  # echo ${name}
+done
