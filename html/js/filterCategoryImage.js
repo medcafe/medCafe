0c49ecd590fd199b94b82c06b90603065fd6581e
Gail Hamilton <ghamilton@mitre.org>
2010-05-17 15:34:02 +0000

    Allow for unfiltering of category

--- html/js/filterCategoryImage.js
+++ html/js/filterCategoryImage.js
@@ -1,25 +1,40 @@
 function filterCategoryImage(filter, tab_num)
 {
 
-	jQuery.each($("#iframe" + tab_num), function() {				
+	jQuery.each($("#iframe" + tab_num), function() {	
+				
 				var source = $(this).attr("src");
 				
 				var pos = source.indexOf("?");
 				var append = "?";
-				
-				if (pos > 0)
+	
+				//If there is no filter specified then this is the UNFILTER event
+				if (filter == "")
 				{
-					append ="&";
-				}
-				var posFilter = source.indexOf("filterCat=");
-				if (posFilter > 0)
-				{
-					source = source + "," + filter;
-				}
+						var posFilter = source.indexOf("filterCat=");
+						if (posFilter > 0)
+						{
+							source = source.substring(0,posFilter);
+						}
+						
+				}	
 				else
-				{
-					source = source + append + "filterCat=" + filter;	
-				}		
+				{		
+					if (pos > 0)
+					{
+						append ="&";
+					}
+					var posFilter = source.indexOf("filterCat=");
+					if (posFilter > 0)
+					{
+						source = source + "," + filter;
+					}
+					else
+					{
+						source = source + append + "filterCat=" + filter;	
+					}		
+				
+				}
 				$(this).attr("src", source);
 				
 	});
