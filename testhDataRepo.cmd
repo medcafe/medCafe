c5df09e4917bb616f3220e6d95890705bcc0d9f4
Jeffrey Hoyt <jchoyt@mitre.org>
2011-01-24 11:58:19 +0000

    Replace i2b2 data for website release

---
+++ testhDataRepo.cmd
@@ -0,0 +1,2 @@
+java -cp medcafe.jar:lib/hData-building-blocks.jar org.mitre.medcafe.util.hDataRepository http://192.168.56.101:8080/hData-REST/resources/hDataRecord/patient/patientinformation/12345.xml
+
