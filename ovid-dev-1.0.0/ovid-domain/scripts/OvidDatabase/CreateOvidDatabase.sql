c3e27b5cdce8e9253f9daf61355c6634f3444772
Mary Greer <mgreer@mitre.org>
2011-05-11 14:34:05 +0000

    Resume version control on directories

--- ovid-dev-1.0.0/ovid-domain/scripts/OvidDatabase/CreateOvidDatabase.sql
+++
@@ -1,8 +0,0 @@
-drop database if exists ovid;
-
-create database ovid;
-
-grant all privileges on ovid.* to 'ovid'@'%' identified by 'ovid';
-grant all privileges on ovid.* to 'ovid'@'localhost' identified by 'ovid';
-
-flush privileges;
